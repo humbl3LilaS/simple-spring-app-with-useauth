fadf6cf539c6314ea47129ee276d648a37bd956d
humbl3LilaS <sabishinekobebe@gmail.com>
2024-09-30 20:23:30 +0630

    initial commit

---
+++ src/main/java/com/simplespringapp/edelweiss/EdelweissApplication.java
@@ -0,0 +1,13 @@
+package com.simplespringapp.edelweiss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdelweissApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdelweissApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplespringapp/edelweiss/EdelweissApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplespringapp.edelweiss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdelweissApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
